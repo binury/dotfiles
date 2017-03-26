0195a2a7cb524511e3a6d08dbbc5cbe6e37512a5
Robin Ury <r.ury@tuta.io>
2017-03-26 01:45:30 -0500

    Added function for automatic Homebrew upkeep and added an exported list of installed brews for easy migration.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -3,3 +3,14 @@
 run-with-sudo() { LBUFFER="sudo $LBUFFER" }
 zle -N run-with-sudo
 bindkey '^Xs' run-with-sudo
+
+brewbump() {
+  echo -e "\nUpdating Homebrew...\n"
+  brew update
+  echo -e "\nUpgrading Homebrew...\n"
+  brew upgrade 
+  echo -e "\nCleaning your mess...\n"
+  brew cleanup
+  brew prune
+  echo -e "\nDone.\n"
+}
