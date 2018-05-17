33b98f8adc4a827e0add4504178049d6ce9d8795
Bin Ury <bin.ury@petrocloud.com>
2018-05-17 13:11:25 -0500

    Prioritize user path to come before system path

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -5,7 +5,18 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
 export ZSH_CACHE="$XDG_CACHE_HOME/zsh"
 mkdir -p $ZSH_CACHE
-export SDKMAN_DIR="/Users/teddy/.sdkman"
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="/usr/local/sbin:$HOME/.opam/system/bin:$HOME/.pyenv:$HOME/.rvm/bin:$HOME/.jenv/bin:/usr/local/bin:$PATH:`yarn global bin`"
 
+export SDKMAN_DIR="$HOME/.sdkman"
+export PYENV_ROOT="$HOME/.pyenv"
+export GOROOT="/usr/local/opt/go/libexec"
+export PEARPATH="$HOME/pear/bin";
+export OPAMPATH="$HOME/.opam/system/bin"
+export PEARPATH="$HOME/pear/bin"
+export RUBYPATH="$HOME/.rvm/bin"
+export YARNPATH=`yarn global bin`;
+export COMPOSER_ROOT="$HOME/.config/composer/vendor/bin"
+export NVM_DIR="$HOME/.nvm"
+export NVM_PATH="/usr/local/opt/nvm/nvm.sh"
+export VSC_ROOT="/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin"
+
+export PATH="/usr/local/sbin:/usr/local/bin:$VSC_ROOT:$OPAMPATH:$OPAMPATH:$PEARPATH:$RUBYPATH:$YARNPATH:$COMPOSER_ROOT:$NVM_PATH:$PATH"
