4a4aaa216de51716e6306eac135d25dea6c695bd
Robin Ury <1146921+teddy-error@users.noreply.github.com>
2021-03-25 09:41:38 -0500

    Change: l{a,l,s} aliases to use exa

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,13 +13,23 @@ alias grep='grep --color=auto'
 
 ##### standard aliases (start with a space to be ignored in history)
 ## default ls is untouched, except coloring
-alias ls=' ls --color=auto'
-alias myls=' ls -C -F -h --color=always'
-alias l=" myls -l"
-alias ll=' myls -l'
-alias la=' myls -lA'
-alias v=" clear; ll -gh"    # standard directory view
-alias vs=" v **/*(.)"         # show all files in all subdirs plain in a list
+# alias ls=' ls --color=auto'
+# alias myls=' ls -C -F -h --color=always'
+# alias l=" myls -l"
+# alias ll=' myls -l'
+# alias la=' myls -lA'
+# alias v=" clear; ll -gh"    # standard directory view
+# alias vs=" v **/*(.)"         # show all files in all subdirs plain in a list
+
+alias ls='exa'
+alias l='exa -lbF --git' # list, size, type, git
+alias ll='exa -lbGF --git' # long list
+alias llm='ll --sort=modified' # modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+# specialty views
+alias lS='exa -1'                                                              # one column, just names
+alias lt='exa --tree --level=2'                                         # tree
 
 alias p=' ps aux | grep'
 alias g='git'
