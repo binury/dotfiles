88e7933eef2e8d4b5c15f6b28587c7176f918c28
Bin Ury <1146921+binury@users.noreply.github.com>
2024-01-28 12:27:17 -0600

    Add: brew auto-update-skip env var

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -1,2 +1,6 @@
+export HOMEBREW_NO_AUTO_UPDATE=1
+#skip periodic dependency upgrade checks
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+
 # brew install zsh-completions
 export FPATH=/usr/local/share/zsh-completions:/usr/local/share/zsh/functions:$FPATH
